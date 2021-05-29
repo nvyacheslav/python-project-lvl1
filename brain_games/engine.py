5f106df5529501b98e6529e3302a61f36fbcb4af
nvyacheslav <vyacheslav.nogacheev@yandex.ru>
2021-05-29 23:11:22 +0700

    add fifth step

---
+++ brain_games/engine.py
@@ -0,0 +1,26 @@
+import prompt
+
+
+def start(game):
+    QUANTITY_OF_ROUNDS = 3
+    number_of_round = 1
+    print('Welcome to the Brain Games!')
+    print(game.DESCRIPTION + '\n')
+    name = prompt.string('May I have your name? ')
+    welcome = f'Hello, {name}'
+    print('\n' + welcome)
+    while number_of_round <= QUANTITY_OF_ROUNDS:
+        result, question, = game.question_and_answer()
+        print('Question: ' + question)
+        answer = prompt.string('Your answer: ')
+        if result == answer:
+            print('Correct!')
+            number_of_round += 1
+        else:
+            print(
+                f'"{answer}" is wrong answer ;(. '
+                f'Correct answer was "{result}".'
+            )
+            print(f"Let's try again, {name}!")
+            return
+    print('Congratulations, ' + name + '!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.engine import start
+from brain_games.games import even
+
+
+def main():
+    start(even)
+
+
+if __name__ == '__main__':
+    main()
