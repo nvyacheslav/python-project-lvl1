1b672d6fc53aaf2cdc0089a149614e1ea9f3b0a7
nvyacheslav <vyacheslav.nogacheev@yandex.ru>
2021-06-26 11:18:43 +0700

    Refactoring #2: changing the logic of asking for the user name

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,10 +10,10 @@ def welcome_user():
 
 def start(game):
     name = welcome_user()
-    print(game.RULES + '\n')    
+    print(game.RULES)    
     quantity_of_rounds = 3
     number_of_round = 1
-    
+
     while number_of_round <= quantity_of_rounds:
         result, question, = game.get_question_and_answer()
         print('Question: ' + question)
